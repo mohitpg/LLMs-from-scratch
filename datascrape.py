fde02d7f2a9655aa4e132952b3930586f33ae0c5
Mohitpg <mohitp.gupta1102@gmail.com>
2024-06-20 23:52:01 +0530

    Added info

--- datascrape.py
+++ datascrape.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup 
 import time
 
-NUMBER_OF_FILES=10 #Max value 40
+NUMBER_OF_FILES=10 #Number of stories in AO3's page -> Max value 40
 
+# Harry Potter fanfics sorted my hits
 listurl = "https://archiveofourown.org/works?work_search%5Bsort_column%5D=hits&include_work_search%5Brating_ids%5D%5B%5D=10&work_search%5Bother_tag_names%5D=&work_search%5Bexcluded_tag_names%5D=&work_search%5Bcrossover%5D=&work_search%5Bcomplete%5D=&work_search%5Bwords_from%5D=&work_search%5Bwords_to%5D=&work_search%5Bdate_from%5D=&work_search%5Bdate_to%5D=&work_search%5Bquery%5D=&work_search%5Blanguage_id%5D=&commit=Sort+and+Filter&tag_id=Harry+Potter+-+J*d*+K*d*+Rowling" 
 html = requests.get(listurl).content 
 data = BeautifulSoup(html, 'html.parser') 
@@ -17,8 +18,8 @@ for x in litags:
 baseurl="https://archiveofourown.org/"
 for i in range(NUMBER_OF_FILES):   
     s=work_name[i].split('_')
-    url=baseurl+'works/'+s[1]+"?view_full_work=true"
-    time.sleep(5)
+    url=baseurl+'works/'+s[1]+"?view_full_work=true" #To extract all text at once
+    time.sleep(5) #Backend scraping cooldown time
     html = requests.get(url).content 
     data = BeautifulSoup(html, 'html.parser') 
     chapters=data.find("div",{"id": "chapters"})
